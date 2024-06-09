9bb5fb131d1a4e311ebc82f1e9a07d23708c239a
Banh26 <118506436+Banh26@users.noreply.github.com>
2024-06-09 20:16:18 +0700

    #4 giaodienp2

--- index.js
+++ index.js
@@ -5,10 +5,10 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import MyCart from './MyCart';
+import MyCart from './src/screens/MyCart';
 import Checkout from './Checkout'
-import notifications from './notificatiions'
-import search from './search'
+import notifications from './src/screens/notificatiions'
+import search from './src/screens/search'
 
 AppRegistry.registerComponent(appName, () => Checkout
 );
